bf6670b155028d16cccf0c7d07185d45e9b0714b
Евгений Мещеряков <e.mescheryakov@i-sys.ru>
2022-04-10 10:36:40 +0400

    settings moved to .env

--- typescript-django/backend/store/views.py
+++ typescript-django/backend/store/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+"""
+high level support for doing this and that.
+"""
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    """my index functions which does nothing"""
+    msg = "Hello World" + str(request.method)
+    return HttpResponse(msg)
